ba472c676caff3b8a27a9a02256c3f2a6fff04e4
nickstu <nickstu85@gmail.com>
2023-06-16 10:21:36 +0200

    refactored more

--- cart_pole.py
+++ cart_pole.py
@@ -7,7 +7,7 @@ from math import cos
 from casadi import *
 
 # parameters
-N = 1000
+N = 200
 delta = 0.01
 l = 1
 m1 = 10
@@ -16,26 +16,16 @@ g = 9.81
 b1 = 0
 b2 = 0
 
-# state
-q1 = 0
-q2 = 0
-q1_d = 0
-q2_d = 0
-
-# trajectories
-u_t = np.zeros(N)
-q1_t = np.zeros(N+1)
-q2_t = np.zeros(N+1)
-q1_t[0] = q1
-q2_t[0] = q2
+# trajectory
+u = np.zeros(N)
 x = np.zeros((4,N+1))
 
 # display
 sz = l/4
 
 # dynamics
-f1 = lambda q1, q2, q1_d, q2_d, u: (l*m2*sin(q2)*q2_d**2 + u + m2*g*cos(q2)*sin(q2)) / (m1 + m2*(1-cos(q2)**2)) - b1*q1_d
-f2 = lambda q1, q2, q1_d, q2_d, u: - (l*m2*cos(q2)*sin(q2)*q2_d**2 + u*cos(q2) + (m1+m2)*g*sin(q2)) / (l*m1 + l*m2*(1-cos(q2)**2)) - b2*q2_d
+f1 = lambda x, u: (l*m2*sin(x[1])*x[3]**2 + u + m2*g*cos(x[1])*sin(x[1])) / (m1 + m2*(1-cos(x[1])**2)) - b1*x[2]
+f2 = lambda x, u: - (l*m2*cos(x[1])*sin(x[1])*x[3]**2 + u*cos(x[1]) + (m1+m2)*g*sin(x[1])) / (l*m1 + l*m2*(1-cos(x[1])**2)) - b2*x[3]
 
 # casadi nonlinear optimization
 opti = Opti()
@@ -44,34 +34,22 @@ U = opti.variable(1,N)
 cost = 0
 
 for i in range(N):
-    opti.subject_to( X[:,i+1] == X[:,i] + delta * vertcat(X[2:4,i], f1(X[0,i], X[1,i], X[2,i], X[3,i], U[0,i]), f2(X[0,i], X[1,i], X[2,i], X[3,i], U[0,i])) )
+    opti.subject_to( X[:,i+1] == X[:,i] + delta * vertcat(X[2:4,i], f1(X[:,i], U[0,i]), f2(X[:,i], U[0,i])) )
     cost += U[0,i]**2
 
-opti.subject_to( X[:,0] == (q1, q2, q1_d, q2_d) )
-opti.subject_to( X[1:4,N] == (math.pi, 0, 0) )
+opti.subject_to( X[:,0] == x[:, 0] )
+opti.subject_to( X[1,N] == math.pi )
+opti.subject_to( X[2:4,N] == (0, 0) )
 
 opti.minimize(cost)
 
 opti.solver("ipopt")
 sol = opti.solve()
-
-u_t = sol.value(U)
+u = sol.value(U)
 
 # integrate
 for i in range(N):
-    u = u_t[i]
-    q1_dd = f1(q1, q2, q1_d, q2_d, u)
-    q2_dd = f2(q1, q2, q1_d, q2_d, u)
-    q1_d += q1_dd * delta
-    q2_d += q2_dd * delta
-    q1 += q1_d * delta;
-    q2 += q2_d * delta;
-    
-    q1_t[i+1] = q1
-    q2_t[i+1] = q2
-    
-q1_t = sol.value(X)[0,:]
-q2_t = sol.value(X)[1,:]
+    x[:,i+1] = x[:,i] + delta * np.hstack((x[2:4,i], f1(x[:,i], u[i]), f2(x[:,i], u[i])))
 
 # display
 def animate(i):
@@ -80,9 +58,9 @@ def animate(i):
     plt.ylim((-1.5*l, 1.5*l))
     plt.gca().set_aspect('equal')
     
-    plt.plot(np.array((q1_t[i] + sz, q1_t[i] + sz, q1_t[i] - sz, q1_t[i] - sz, q1_t[i] + sz)),  np.array((sz, -sz, -sz, sz, sz)))
-    plt.gca().add_patch(plt.Circle((q1_t[i]+math.sin(q2_t[i]), -math.cos(q2_t[i])), sz/2, color='blue'))
-    plt.plot(np.array((q1_t[i], q1_t[i]+math.sin(q2_t[i]))), np.array((0, -math.cos(q2_t[i]))))
+    plt.plot(x[0,:][i] + np.array((sz, sz, - sz, - sz, + sz)),  np.array((sz, -sz, -sz, sz, sz)))
+    plt.gca().add_patch(plt.Circle((x[0,:][i] + sin(x[1,:][i]), - cos(x[1,:][i])), sz/2, color='blue'))
+    plt.plot(np.array((x[0,:][i], x[0,:][i] + sin(x[1,:][i]))), np.array((0, - cos(x[1,:][i]))))
 
 ani = FuncAnimation(plt.gcf(), animate, frames=N+1, repeat=False, interval=0)
 
