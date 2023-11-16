4a477ddfa09a99bf05b870446134b5cea0c5dd1b
nickstu <nickstu85@gmail.com>
2023-11-16 16:48:34 +0100

    added cart pole ddp

--- cart_pole_ddp.py
+++ cart_pole_ddp.py
@@ -1,79 +1,63 @@
 import numpy as np
+import math
 import matplotlib.pyplot as plt
-from casadi import *
-from sympy import *
+import casadi as cs
+from matplotlib.animation import FuncAnimation
 
 delta = 0.1
+n, m = (4, 1)
 N = 100
-Q = np.eye(3)
-R = np.eye(2)
-Qter = 100 * np.eye(3)
+Q = np.eye(n)
+R = np.eye(m)
+Qter = 100 * np.eye(n)
 iterations = 100
-n = 3
-m = 2
+g = 9.81
+
 alpha = 0.9
 
 x = np.zeros((n, N+1))
-x[:, 0] = np.array([0, 1, 0])
+x[:, 0] = np.array([0, 0, 0, 0])
 
-topt = Opti()
-X1 = topt.variable(1)
-X2 = topt.variable(1)
-X3 = topt.variable(1)
-X = np.array((X1, X2, X3))
-U = np.array((topt.variable(1)))
+opt = cs.Opti()
+X = opt.variable(4)
+U = opt.variable(1)
 
-f = lambda x, u: x + delta * np.array([u[0] * np.cos(x[2]), u[0] * np.sin(x[2]), u[1]])
-L = lambda x, u: x.T @ Q @ x + u.T @ R @ u
-L_ter = lambda x_ter: x_ter.T @ Qter @ x_ter
+l, m1, m2, b1, b2 = (1, 10, 5, 0, 0)
+f1 = lambda x, u: (l*m2*cs.sin(x[1])*x[3]**2 + u + m2*g*cs.cos(x[1])*cs.sin(x[1])) / (m1 + m2*(1-cs.cos(x[1])**2)) - b1*x[2]
+f2 = lambda x, u: - (l*m2*cs.cos(x[1])*cs.sin(x[1])*x[3]**2 + u*cs.cos(x[1]) + (m1+m2)*g*cs.sin(x[1])) / (l*m1 + l*m2*(1-cs.cos(x[1])**2)) - b2*x[3]
+f_ = lambda x, u: x + delta * cs.vertcat( x[2:4], f1(x, u), f2(x, u) )
+#f_ = lambda x, u: x + delta * cs.vertcat( u[0] * np.cos(x[2]), u[0] * np.sin(x[2]), u[1] )
+L_ = lambda x, u: x.T @ Q @ x + u.T @ R @ u
+#L_ter_ = lambda x_ter: (x_ter - np.array([math.pi, 0, 0, 0])).T @ Qter @ (x_ter - np.array([math.pi, 0, 0, 0]))
+L_ter_ = lambda x_ter: 100 * (x_ter[0]**2 + x_ter[2]**2 + x_ter[3]**2)
+#L_ter_ = lambda x_ter: x_ter.T @ Qter @ x_ter
 
-XX = np.array((symbols('X1'), symbols('X2'), symbols('X3')))
-UU = np.array((symbols('UU')))
-print(f(XX, UU).diff(XX))
+f = cs.Function('f', [X, U], [f_(X,U)])
+L = cs.Function('L', [X, U], [L_(X,U)])
+L_ter = cs.Function('L_ter', [X], [L_ter_(X)])
+fx = cs.Function('fx', [X, U], [cs.jacobian(f_(X,U), X)])
+fu = cs.Function('fu', [X, U], [cs.jacobian(f_(X,U), U)])
+Lx = cs.Function('Lx', [X, U], [cs.jacobian(L_(X,U), X)])
+Lu = cs.Function('Lu', [X, U], [cs.jacobian(L_(X,U), U)])
+Lxx = cs.Function('Lxx', [X, U], [cs.jacobian(cs.jacobian(L_(X,U), X).T, X)])
+Lux = cs.Function('Lux', [X, U], [cs.jacobian(cs.jacobian(L_(X,U), U).T, X)])
+Luu = cs.Function('Luu', [X, U], [cs.jacobian(cs.jacobian(L_(X,U), U).T, U)])
+L_terx = cs.Function('L_terx', [X], [cs.jacobian(L_ter_(X), X)])
+L_terxx = cs.Function('L_terxx', [X], [cs.jacobian(cs.jacobian(L_ter_(X), X), X)])
 
-FX = jacobian(f(X,U),X)
-FU = jacobian(f(X,U),U)
-LX = jacobian(L(X,U),X)
-LU = jacobian(L(X,U),U)
-LXX = jacobian(LX,X)
-LUX = jacobian(LU,X)
-LUU = jacobian(LU,U)
-L_TERX = jacobian(L_ter(X,U),X)
-L_TERXX = jacobian(L_TERX,X)
-
-fu = lambda x, u: FX(x, u)
-fu = lambda x, u: FU(x, u)
-Lx = lambda x, u: LX(x, u)
-Lu = lambda x, u: FU(x, u)
-Lxx = lambda x, u: LXX(x, u)
-Lux = lambda x, u: LUX(x, u)
-Luu = lambda x, u: LUU(x, u)
-L_terx = lambda x_ter: L_TERX(x_ter)
-L_terxx = lambda x_ter: L_TERXX(x_ter)
-
-"""fx = lambda x, u: np.eye(n) + delta * np.array([[0, 0, -u[0] * np.sin(x[2])], [0, 0, u[0] * np.cos(x[2])], [0, 0, 0]])
-fu = lambda x, u: delta * np.array([[np.cos(x[2]), 0], [np.sin(x[2]), 0], [0, 1]])
-Lx = lambda x, u: 2 * Q @ x
-Lu = lambda x, u: 2 * R @ u
-Lxx = lambda x, u: 2 * Q
-Lux = lambda x, u: np.zeros((m, n))
-Luu = lambda x, u: 2 * R
-L_terx = lambda x_ter: 2 * Qter @ x_ter
-L_terxx = lambda x_ter: 2 * Qter"""
-
-u = np.array([0.15 * np.ones(N), -0.3 * np.ones(N)])
+u = np.array([1 * np.ones(N)])
 
 cost = 0
 for i in range(N):
-  x[:, i+1] = f(x[:, i], u[:, i])
-  cost = cost + L(x[:, i], u[:, i])
+  x[:, i+1] = np.array(f(x[:,i], u[:,i])).flatten()
+  cost = cost + L(x[:, i], u[:,i])
 
 cost = cost + L_ter(x[:, N])
 
 k = [np.array((m, 1))] * (N+1)
 K = [np.array((m, n))] * (N+1)
 
-plt.figure()
+#plt.figure()
 
 for iter in range(iterations):
   V = np.zeros(N+1)
@@ -84,18 +68,18 @@ for iter in range(iterations):
   Vxx[:, :, N] = L_terxx(x[:, N])
 
   for i in reversed(range(N)):
-    Qx = Lx(x[:, i], u[:, i]) + fx(x[:, i], u[:, i]).T @ Vx[:, i + 1]
-    Qu = Lu(x[:, i], u[:, i]) + fu(x[:, i], u[:, i]).T @ Vx[:, i + 1]
+    Qx = np.array(Lx(x[:, i], u[:,i])).flatten() + fx(x[:, i], u[:,i]).T @ Vx[:, i + 1]
+    Qu = np.array(Lu(x[:, i], u[:,i])).flatten() + fu(x[:, i], u[:,i]).T @ Vx[:, i + 1]
 
-    Qxx = Lxx(x[:, i], u[:, i]) + fx(x[:, i], u[:, i]).T @ Vxx[:, :, i + 1] @ fx(x[:, i], u[:, i])
-    Qux = Lux(x[:, i], u[:, i]) + fu(x[:, i], u[:, i]).T @ Vxx[:, :, i + 1] @ fx(x[:, i], u[:, i])
-    Quu = Luu(x[:, i], u[:, i]) + fu(x[:, i], u[:, i]).T @ Vxx[:, :, i + 1] @ fu(x[:, i], u[:, i])
+    Qxx = Lxx(x[:, i], u[:,i]) + fx(x[:, i], u[:,i]).T @ Vxx[:, :, i + 1] @ fx(x[:, i], u[:,i])
+    Qux = Lux(x[:, i], u[:,i]) + fu(x[:, i], u[:,i]).T @ Vxx[:, :, i + 1] @ fx(x[:, i], u[:,i])
+    Quu = Luu(x[:, i], u[:,i]) + fu(x[:, i], u[:,i]).T @ Vxx[:, :, i + 1] @ fu(x[:, i], u[:,i])
 
     k[i] = -np.linalg.inv(Quu) @ Qu
     K[i] = -np.linalg.inv(Quu) @ Qux
 
     V[i] = V[i + 1] - 0.5 * k[i].T @ Quu @ k[i]
-    Vx[:, i] = Qx - K[i].T @ Quu @ k[i]
+    Vx[:, i] = np.array(Qx - K[i].T @ Quu @ k[i]).flatten()
     Vxx[:, :, i] = Qxx - K[i].T @ Quu @ K[i]
 
   xnew = np.zeros((n, N+1))
@@ -103,22 +87,34 @@ for iter in range(iterations):
   unew = np.zeros((m, N))
 
   for i in range(N):
-    unew[:, i] = u[:, i] + alpha * k[i] + K[i] @ (xnew[:, i] - x[:, i])
-    xnew[:, i+1] = f(xnew[:, i], unew[:, i])
+    unew[:, i] = u[:,i] + alpha * k[i] + K[i] @ (xnew[:, i] - x[:, i])
+    xnew[:, i+1] = np.array(f(xnew[:, i], unew[:, i])).flatten()
 
   u = unew
   x = xnew
 
-  plt.plot(x[0, :], x[1, :], color=[0, 0, 1, iter / iterations])
+  #plt.plot(x[0, :], x[1, :], color=[0, 0, 1, iter / iterations])
 
 xcheck = np.zeros((n, N+1))
-xcheck[:, 0] = np.array([0, 1, 0])
+xcheck[:, 0] = np.array([0, 0, 0, 0])
 for i in range(N):
-  xcheck[:, i+1] = f(xcheck[:, i], u[:, i])
+  xcheck[:, i+1] = np.array(f(xcheck[:, i], u[:, i])).flatten()
 
-plt.plot(xcheck[0, :], xcheck[1, :], 'k')
-plt.axis('equal')
-plt.axis([-1.5, 1.5, -0.5, 1.5])
+#plt.plot(xcheck[0, :], xcheck[1, :], 'k')
+#plt.axis([-0.5, 3.5, -0.5, 1.5])
 
+def animate(i):
+  np.set_printoptions(precision=2)
+
+  plt.clf()
+  plt.xlim((-5*l, 5*l))
+  plt.ylim((-1.5*l, 1.5*l))
+  plt.gca().set_aspect('equal')
+    
+  plt.plot(x[0,:][i] + np.array((l, l, - l, - l, + l))/4,  np.array((l, -l, -l, l, l))/4)
+  plt.gca().add_patch(plt.Circle((x[0,:][i] + math.sin(x[1,:][i]), - math.cos(x[1,:][i])), l/8, color='blue'))
+  plt.plot(np.array((x[0,:][i], x[0,:][i] + math.sin(x[1,:][i]))), np.array((0, - math.cos(x[1,:][i]))))
+
+ani = FuncAnimation(plt.gcf(), animate, frames=N+1, repeat=False, interval=0)
 plt.show()
 
