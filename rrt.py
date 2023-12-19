bb7dc3b99816059e07daeaf411c2e1d39251cbfe
nickstu <nickstu85@gmail.com>
2023-12-19 12:44:21 +0100

    added stuff

--- rrt.py
+++ rrt.py
@@ -14,13 +14,24 @@ class Node:
     self.x = x
     self.u = u
 
+def find_closest_node(goal, list):
+  distance = 10e6
+  closest_node = None
+  for node in list:
+    goal_distance = (goal - node.x).T @ (goal - node.x)
+    if goal_distance < distance:
+      distance = goal_distance
+      closest_node = node
+  return closest_node
+
 delta = 0.01
 n, m = (4, 1)
-iterations = 1000
-goal_radius = 0.1
+iterations = 2000
+goal_radius = 0.2
 max_input = 100
 max_state = 10
-primitives = [-max_input, max_input]
+primitives = np.array([-1, -0.5, 0, 0.5, 1]) * max_input
+initial_exploration_rate = 1.0
 
 x_ini = np.array([0, 0, 0, 0])
 x_ter = np.array([math.pi, 0, 0, 0])
@@ -36,22 +47,24 @@ total_time = 0
 for iter in range(iterations):
   start_time = time.time()
 
-  # sample the state space
-  sample = (np.random.rand(n) - 0.5) * 2 * max_state
-
-  # find closest node
-  distance = 10e6
-  closest_node = None
-  for node in tree:
-    sample_distance = np.linalg.norm(sample - node.x)
-    if sample_distance < distance:
-      distance = sample_distance
-      closest_node = node
+  exploration_rate = initial_exploration_rate * float(iterations - iter) / float(iterations)
 
   # expand
-  random_input = primitives[random.randrange(len(primitives))]
-  new_state = closest_node.x + delta * np.array(f(closest_node.x, random_input)).flatten()
-  new_node = Node(new_state, random_input, closest_node)
+  if random.randrange(0,1) < exploration_rate:
+    sample = (np.random.rand(n) - 0.5) * 2 * max_state
+    closest_node = find_closest_node(sample, tree)
+    random_input = primitives[random.randrange(len(primitives))]
+    new_state = closest_node.x + delta * np.array(f(closest_node.x, random_input)).flatten()
+    new_node = Node(new_state, random_input, closest_node)
+  else:
+    closest_node = find_closest_node(x_ter, tree)
+    final_nodes = []
+    for input in primitives:
+      new_state = closest_node.x + delta * np.array(f(closest_node.x, input)).flatten()
+      print(new_state)
+      final_nodes.append(Node(new_state, input, closest_node))
+    new_node = find_closest_node(x_ter, final_nodes)
+
   closest_node.children.append(new_node)
   tree.append(new_node)
 
@@ -64,20 +77,9 @@ for iter in range(iterations):
   total_time += elapsed_time
   print('Iteration ', iter, ' time: ', elapsed_time*1000, ' ms')
 
-# find best node
-distance = 10e6
-best_node = None
-for node in tree:
-  goal_distance = np.linalg.norm(x_ter - node.x)
-  print(goal_distance)
-  if goal_distance < distance:
-    distance = goal_distance
-    best_node = node
-
-print('best ', best_node.x)
-
+# find best trajectory
 u_backwards = []
-search_node = best_node
+search_node = closest_node = find_closest_node(x_ter, tree)
 while search_node.parent != None:
   u_backwards.append(search_node.u)
   search_node = search_node.parent
