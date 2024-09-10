f2c639df5e8ce88eadc3dfc39d06afc66c49b287
Nicola Scianca <nickstu85@gmail.com>
2024-09-10 10:52:32 +0200

    small changes to sqp

--- sqp.py
+++ sqp.py
@@ -5,48 +5,45 @@ import time
 import model
 import animation
 
+# parameters
 delta = 0.01
 n, m = (4, 1)
 N = 100
 Q = np.eye(n)
 R = np.eye(m)
 Qter = np.eye(n)
-iterations = 100
-
+iterations = 10
+total_time = 0
 x_init = np.array([0, 0, 0, 0])
 x_goal = np.array([cs.pi, 0, 0, 0])
 
-x = np.zeros((n, N+1))
+# initial guess
+x = np.zeros((n,N+1))
+u = np.zeros((m,N))
 x[:, 0] = x_init
 
-opt_sym = cs.Opti()
-X_ = opt_sym.variable(4)
-U_ = opt_sym.variable(1)
-
+# dynamics and its derivatives
 #ff, p = model.get_cart_pendulum_model()
 ff, p = model.get_pendubot_model()
 f_ = lambda x, u: x + delta * ff(x, u)
 
+opt_sym = cs.Opti()
+X_ = opt_sym.variable(n)
+U_ = opt_sym.variable(m)
 f  = cs.Function('f' , [X_, U_], [f_(X_,U_)]                 , {"post_expand": True})
 fx = cs.Function('fx', [X_, U_], [cs.jacobian(f_(X_,U_), X_)], {"post_expand": True})
 fu = cs.Function('fu', [X_, U_], [cs.jacobian(f_(X_,U_), U_)], {"post_expand": True})
-
-u = np.ones(N) * 1
-for i in range(N):
-  x[:, i+1] = np.array(f(x[:,i], u[i])).flatten()
-
-total_time = 0
 
 # optimization problem
 opt = cs.Opti('conic')
 opt.solver('proxqp')
 
-dX = opt.variable(4,N+1)
-dU = opt.variable(1,N)
-X = opt.parameter(4,N+1)
-U = opt.parameter(1,N)
+dX = opt.variable(n,N+1)
+dU = opt.variable(m,N)
+X = opt.parameter(n,N+1)
+U = opt.parameter(m,N)
 
-opt.subject_to( dX[:,0] == (0,0,0,0) )
+opt.subject_to( dX[:,0] == np.zeros(n) )
 opt.subject_to( X[:,N] + dX[:,N] == x_goal )
 for i in range(N):
   opt.subject_to( X[:,i+1] + dX[:,i+1] == f(X[:,i], U[:,i]) + \
@@ -61,6 +58,7 @@ for i in range(N):
 
 opt.minimize(cost)
 
+# SQP iterations
 for iter in range(iterations):
   start_time = time.time()
 
@@ -75,7 +73,7 @@ for iter in range(iterations):
   total_time += elapsed_time
   print('Iteration time: ', elapsed_time*1000, ' ms')
 
-xcheck = np.zeros((n, N+1))
+xcheck = np.zeros((n,N+1))
 xcheck[:,0] = x_init
 for i in range(N):
   xcheck[:, i+1] = np.array(f(xcheck[:,i], u[i])).flatten()
